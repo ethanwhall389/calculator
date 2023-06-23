c5ddc16e11987ec1334d19c7ef58d17fbe9ad4a6
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-06-23 12:58:37 +0000

    Add error message if user divides by 0

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ clearBttn.addEventListener('click', () => {
 })
 
 function finalEvaluate () {
-    if (operator.length > 0) {
+    if (operator.length > 0 && operandTwo.length > 0) {
         // We have enough for a full equation: operate on them.
         let oldOperandOne = operandOne;
         let equalSign = '='
@@ -99,4 +99,51 @@ function updateSmallDisplay () {
         smallDisplayText += `${arguments[i]} `;
     }
     smallDisplayPara.textContent = smallDisplayText;
+}
+
+
+
+// MATH FUNCTIONS SINCE CODESPACES
+
+function add () {
+    let final = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        final += arguments[i];
+    }
+    return final;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    if (b === 0) {
+        return ('You can\'t divide by 0!');
+    }
+    return a / b;
+}
+
+
+function operate (a, op, b) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+            break;
+        case '−':
+            return subtract(a, b);
+            break;
+        case String.fromCharCode(215):
+            return multiply(a, b);
+            break;
+        case String.fromCharCode(247):
+            return divide(a, b);
+            break;
+        default:
+            return 'There has been an error.';
+    }
 }
