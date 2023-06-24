470a68f23c9d4e40682ce873ec5c939bf54216dd
Ethan Hall <ethanwhall@icloud.com>
2023-06-24 12:17:00 -0400

    Add dark mode with toggle button

--- js/color-modes.js
+++ js/color-modes.js
@@ -1,9 +1,7 @@
 let root = document.documentElement;
 
-let colorSwitch = document.querySelector('.switch-color-mode');
+let colorSwitch = document.querySelector('input');
 
-let lightMode = true;
-let darkMode = false;
 
 colorSwitch.addEventListener('click', () => {
     switchColors();
@@ -11,34 +9,38 @@ colorSwitch.addEventListener('click', () => {
 
 
 function switchColors() {
-    if (lightMode) {
+    if (colorSwitch.checked == true) {
         //run dark mode
         root.style.setProperty('--main-bg-color', 'black');
+        root.style.setProperty('--main-header-footer-color', 'rgb(34, 34, 47)');
         root.style.setProperty('--main-text-color', 'white');
         root.style.setProperty('--main-display-color', 'black');
-        root.style.setProperty('--main-calc-color', 'black');
+        root.style.setProperty('--main-calc-color', 'rgb(34, 34, 47)');
         root.style.setProperty('--main-bttn-color', 'black');
-        root.style.setProperty('--main-bttn-hover', 'black');
+        root.style.setProperty('--main-bttns-text-color', 'white');
+        root.style.setProperty('--main-bttn-hover', 'rgb(28, 28, 28)');
         root.style.setProperty('--operator-bttns-color', 'black');
-        root.style.setProperty('--operator-bttns-text-color', 'black');
+        root.style.setProperty('--operator-bttns-text-color', 'white');
         root.style.setProperty('--equals-bttn-color', 'black');
         root.style.setProperty('--clear-bttn-color', 'black');
-        darkMode = true;
-        lightMode = false
+        root.style.setProperty('--main-link-color', 'rgba(253, 189, 57)');
 
-    } else if (darkMode){
+
+    } else if (colorSwitch.checked == false){
         //run light mode
         root.style.setProperty('--main-bg-color', 'rgb(253, 253, 250)');
+        root.style.setProperty('--main-header-footer-color', 'rgba(253, 189, 57)');
         root.style.setProperty('--main-text-color', 'black');
         root.style.setProperty('--main-display-color', 'rgb(253, 253, 250)');
         root.style.setProperty('--main-calc-color', 'rgb(253, 189, 57)');
         root.style.setProperty('--main-bttn-color', 'rgb(247, 247, 247)');
+        root.style.setProperty('--main-bttns-text-color', 'black');
         root.style.setProperty('--main-bttn-hover', 'rgb(226, 226, 226)');
         root.style.setProperty('--operator-bttns-color', 'black');
         root.style.setProperty('--operator-bttns-text-color', 'white');
         root.style.setProperty('--equals-bttn-color', 'rgb(135, 229, 119)');
         root.style.setProperty('--clear-bttn-color', 'rgb(230, 65, 36)');
-        lightMode = true;
-        darkMode = false;
+        root.style.setProperty('--main-link-color', 'white');
+
     }
 }
